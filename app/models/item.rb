1f88d52203726a95cec9b02e010a97da21f3af5b
hikaru-kitasaki <pikapika0200@gmail.com>
2020-12-11 15:33:56 +0900

    コードの簡略化

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-
-  validates :image,               presence: true
-  validates :product_name,        presence: true
-  validates :product_description, presence: true
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :product_description
+  end
   validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   validates :selling_price, presence: true, format: { with: /\A[0-9]+\z/ }
 
@@ -14,9 +15,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_charges
   belongs_to_active_hash :shipping_area
   belongs_to_active_hash :days_to_ship
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :shipping_charges_id, numericality: { other_than: 1 }
-  validates :shipping_area_id, numericality: { other_than: 1 }
-  validates :days_to_ship_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_charges_id
+    validates :shipping_area_id 
+    validates :days_to_ship_id 
+  end
 end
