bb10ec74269b3eea5ce9bbbe68ac3521be12afe8
hikaru-kitasaki <pikapika0200@gmail.com>
2020-12-11 18:26:03 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,9 @@ class Item < ApplicationRecord
     validates :image
     validates :product_name
     validates :product_description
-    validates :selling_price,format: { with: /\A[0-9]+\z/ }
+    validates :selling_price, format: { with: /\A[0-9]+\z/ }
   end
-  
+
   validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 
   with_options numericality: { other_than: 1 } do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
-  end    
+  end
 
   describe '商品出品登録' do
     context '商品出品ができる時' do
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
     end
-    
+
     context '商品出品ができない時' do
       it 'imageが空だと登録できない' do
         @item.image = nil
@@ -31,53 +31,53 @@ RSpec.describe Item, type: :model do
       it 'category_idが1だと登録できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it 'status_idが1だと登録できない' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be other than 1")
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it 'shipping_charges_idが1だと登録できない' do
         @item.shipping_charges_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping charges must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping charges must be other than 1')
       end
       it 'shipping_area_idが1だと登録できない' do
         @item.shipping_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
       end
       it 'days_to_ship_idが1だと登録できない' do
         @item.days_to_ship_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
+        expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
       end
       it 'selling_priceが空だと登録できない' do
         @item.selling_price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is invalid")
+        expect(@item.errors.full_messages).to include('Selling price is invalid')
       end
       it 'selling_priceが299円以下だと登録できない' do
         @item.selling_price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Selling price must be greater than or equal to 300')
       end
       it 'selling_Priceが10000000円以上だと登録できない' do
-        @item.selling_price = 10000000
+        @item.selling_price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Selling price must be less than or equal to 9999999')
       end
       it 'selling_priceが半角数字でないと登録できない' do
         @item.selling_price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is not a number")
+        expect(@item.errors.full_messages).to include('Selling price is not a number')
       end
       it 'userが紐付いていないと登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
-  end    
+  end
 end
