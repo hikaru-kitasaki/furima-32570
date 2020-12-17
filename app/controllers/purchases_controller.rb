7430605f35467106c88e25921b4e7a37a810debc
hikaru-kitasaki <pikapika0200@gmail.com>
2020-12-17 12:46:48 +0900

    コントローラーの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,16 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    if Purchase.exists?
+      redirect_to root_path
+    else
+      if current_user.id == @item.user_id
+        redirect_to root_path
+      else
+        @order = Order.new
+      end
+    end
   end
 
   def create
